d1163f6e3f443f645eeb3fbbc5c4c4b55d760bff
Fanny Fortier <e1249348@cmaisonneuve.qc.ca>
2026-05-07 16:07:03 -0400

    Intéractivité clic avec les objets de l'énigme

---
+++ Assets/scripts/enigme_manoir/objetPesable.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class objetPesable : MonoBehaviour
+{
+
+    [Header("Poids")]
+    [SerializeField] private int _valeurPoids = 1;
+
+    public int valeurPoids => _valeurPoids;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ModifierPoids(float multiplicateur)
+    {
+        _valeurPoids = Mathf.Max(1, Mathf.RoundToInt(_valeurPoids * multiplicateur));
+        Debug.Log($"[ObjetPesable] {gameObject.name} - nouveau poids : {_valeurPoids}");
+    }
+}
